71d6a11e59c9353aab4e4dd89793d0d572182861
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-06 10:31:37 +0100

    Add lines of code counter
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- tools/count_datasets.py
+++ tools/count_datasets.py
@@ -1,5 +1,16 @@
 import os
 
+import os
+
+def count_total_lines():
+    total_lines = 0
+    for root, dirs, files in os.walk("dataset/"):
+        for file in files:
+            if file.startswith("code."):
+                with open(os.path.join(root, file)) as f:
+                    total_lines += sum(1 for line in f if line.strip() and not line.startswith("#"))
+    return total_lines
+
 if __name__ == "__main__":
     count = 0
     for file in os.listdir("dataset"):
@@ -9,5 +20,6 @@ if __name__ == "__main__":
         readme = f.read()
     lines = readme.split("\n")
     lines[4] = "There are `"+str(count)+"` datasets in this repository"
+    lines[5] = "There is a total of `"+str(count_total_lines())+"` lines of code in this repository"
     with open("README.md", "w") as f:
         f.write("\n".join(lines))
