ad8ca15793dd76defd45c7ff123bc9b6333c216f
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-06-16 18:54:21 +0100

    Add to repository
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

---
+++ repository/1689/code.py
@@ -0,0 +1,110 @@
+# Genetic Algorithm for Traveling Salesman Problem
+
+import random
+import math
+
+# Constants
+POPULATION_SIZE = 100
+ELITE_SIZE = 10
+MUTATION_RATE = 0.01
+NUM_GENERATIONS = 1000
+
+# City class
+class City:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance_to(self, city):
+        x_distance = abs(self.x - city.x)
+        y_distance = abs(self.y - city.y)
+        return math.sqrt(x_distance**2 + y_distance**2)
+
+# Helper functions
+def generate_random_route(city_list):
+    return random.sample(city_list, len(city_list))
+
+def calculate_fitness(route):
+    total_distance = 0
+    for i in range(len(route)):
+        from_city = route[i]
+        to_city = route[(i + 1) % len(route)]
+        total_distance += from_city.distance_to(to_city)
+    return 1 / total_distance
+
+def crossover(parent1, parent2):
+    start_index = random.randint(0, len(parent1))
+    end_index = random.randint(start_index + 1, len(parent1))
+    child = parent1[start_index:end_index]
+
+    for city in parent2:
+        if city not in child:
+            child.append(city)
+
+    return child
+
+def mutate(route):
+    for i in range(len(route)):
+        if random.random() < MUTATION_RATE:
+            j = random.randint(0, len(route))
+            route[i], route[j] = route[j], route[i]
+    return route
+
+# Genetic Algorithm
+def genetic_algorithm(city_list):
+    population = []
+    for _ in range(POPULATION_SIZE):
+        population.append(generate_random_route(city_list))
+
+    for generation in range(NUM_GENERATIONS):
+        population = sorted(population, key=lambda x: calculate_fitness(x), reverse=True)
+        elite = population[:ELITE_SIZE]
+
+        for _ in range(POPULATION_SIZE - ELITE_SIZE):
+            parent1 = random.choice(elite)
+            parent2 = random.choice(elite)
+            child = crossover(parent1, parent2)
+            child = mutate(child)
+            population.append(child)
+
+        population = population[:POPULATION_SIZE]
+
+    best_route = population[0]
+    return best_route
+
+# Example usage
+def main():
+    # Create a list of cities
+    cities = [
+        City(60, 200),
+        City(180, 200),
+        City(80, 180),
+        City(140, 180),
+        City(20, 160),
+        City(100, 160),
+        City(200, 160),
+        City(140, 140),
+        City(40, 120),
+        City(100, 120),
+        City(180, 100),
+        City(60, 80),
+        City(120, 80),
+        City(180, 60),
+        City(20, 40),
+        City(100, 40),
+        City(200, 40),
+        City(20, 20),
+        City(60, 20),
+        City(160, 20)
+    ]
+
+    # Solve TSP using genetic algorithm
+    best_route = genetic_algorithm(cities)
+
+    # Print the best route
+    print("Best Route:")
+    for city in best_route:
+        print(city.x, city.y)
+
+if __name__ == "__main__":
+    main()
---
+++ repository/3841/code.c
@@ -0,0 +1,115 @@
+// Image Segmentation using K-means Clustering
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAX_ITERATIONS 100
+#define NUM_CLUSTERS 3
+
+typedef struct {
+    float r, g, b;
+} Pixel;
+
+typedef struct {
+    float r, g, b;
+    int count;
+} Cluster;
+
+// Function to calculate Euclidean distance between two pixels
+float calculate_distance(Pixel pixel1, Pixel pixel2) {
+    float distance = sqrt(pow(pixel1.r - pixel2.r, 2) + pow(pixel1.g - pixel2.g, 2) + pow(pixel1.b - pixel2.b, 2));
+    return distance;
+}
+
+// Function to assign pixels to the closest cluster
+void assign_to_clusters(Pixel* pixels, Cluster* clusters, int num_pixels) {
+    for (int i = 0; i < num_pixels; i++) {
+        float min_distance = calculate_distance(pixels[i], clusters[0]);
+        int min_cluster = 0;
+
+        for (int j = 1; j < NUM_CLUSTERS; j++) {
+            float distance = calculate_distance(pixels[i], clusters[j]);
+            if (distance < min_distance) {
+                min_distance = distance;
+                min_cluster = j;
+            }
+        }
+
+        clusters[min_cluster].r += pixels[i].r;
+        clusters[min_cluster].g += pixels[i].g;
+        clusters[min_cluster].b += pixels[i].b;
+        clusters[min_cluster].count++;
+    }
+}
+
+// Function to update the cluster centers
+void update_cluster_centers(Cluster* clusters) {
+    for (int i = 0; i < NUM_CLUSTERS; i++) {
+        if (clusters[i].count > 0) {
+            clusters[i].r /= clusters[i].count;
+            clusters[i].g /= clusters[i].count;
+            clusters[i].b /= clusters[i].count;
+        }
+    }
+}
+
+// Function to perform image segmentation using K-means clustering
+void perform_segmentation(Pixel* pixels, int width, int height) {
+    Cluster clusters[NUM_CLUSTERS];
+
+    // Initialize cluster centers randomly
+    for (int i = 0; i < NUM_CLUSTERS; i++) {
+        clusters[i].r = (float)rand() / RAND_MAX;
+        clusters[i].g = (float)rand() / RAND_MAX;
+        clusters[i].b = (float)rand() / RAND_MAX;
+        clusters[i].count = 0;
+    }
+
+    // Iterate until convergence or maximum iterations
+    int iterations = 0;
+    while (iterations < MAX_ITERATIONS) {
+        assign_to_clusters(pixels, clusters, width * height);
+        update_cluster_centers(clusters);
+        iterations++;
+    }
+
+    // Assign each pixel to the cluster with the closest center
+    for (int i = 0; i < width * height; i++) {
+        float min_distance = calculate_distance(pixels[i], clusters[0]);
+        int min_cluster = 0;
+
+        for (int j = 1; j < NUM_CLUSTERS; j++) {
+            float distance = calculate_distance(pixels[i], clusters[j]);
+            if (distance < min_distance) {
+                min_distance = distance;
+                min_cluster = j;
+            }
+        }
+
+        // Set pixel color based on the cluster center
+        pixels[i].r = clusters[min_cluster].r;
+        pixels[i].g = clusters[min_cluster].g;
+        pixels[i].b = clusters[min_cluster].b;
+    }
+}
+
+// Example usage
+int main() {
+    // Read image pixels (replace with your image loading logic)
+    int width = 640;
+    int height = 480;
+    Pixel* pixels = (Pixel*)malloc(width * height * sizeof(Pixel));
+
+    // Populate pixels with image data
+
+    // Perform image segmentation
+    perform_segmentation(pixels, width, height);
+
+    // Process the segmented image (e.g., save, display, or further analysis)
+
+    // Cleanup
+    free(pixels);
+
+    return 0;
+}
